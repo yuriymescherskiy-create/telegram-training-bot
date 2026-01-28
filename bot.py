f8154bf3f741ffd4f00598c3fd7b6d95b3f6fed2
yuriymescherskiy-create <yuriy.mescherskiy@gmail.com>
2026-01-28 16:15:53 +0500

    Create bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from aiogram import Bot, Dispatcher, executor
+from config import BOT_TOKEN
+from handlers.start import register_start
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+register_start(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
+
 
